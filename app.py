c4317c496c41593d08dcd656887e58419777b1c4
Gabriel Gergye <143243032+GabrielGeeergye@users.noreply.github.com>
2024-09-30 20:33:11 -0300

    Aula 1

---
+++ app.py
@@ -0,0 +1,13 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+#Criando uma variavel
+#Nele não é necessário colocar o tipo da variavel
+opcao_escolhida = input('Escolha uma opção: ')
+
+# Diferença do "" e ''
+print('Você escolheu a opção:', opcao_escolhida)
